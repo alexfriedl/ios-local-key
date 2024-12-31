be30f310821ffa11cfa08b429e351f15145f6dad
Alexander Friedl <hi@alexfriedl.com>
2024-12-31 14:02:22 +0100

    initial

---
+++ LocalKey/LocalKeyApp.swift
@@ -0,0 +1,32 @@
+//
+//  LocalKeyApp.swift
+//  LocalKey
+//
+//  Created by Alexander Friedl on 20.12.24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct LocalKeyApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ LocalKeyTests/LocalKeyTests.swift
@@ -0,0 +1,16 @@
+//
+//  LocalKeyTests.swift
+//  LocalKeyTests
+//
+//  Created by Alexander Friedl on 20.12.24.
+//
+
+import Testing
+
+struct LocalKeyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
